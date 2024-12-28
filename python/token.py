3720bda54af6bdaf50016a0a98ceae486b8bef20
Alvaro Macedo <alvaro.macedo@nubank.com.br>
2024-12-28 19:06:01 -0300

    Adding token types

--- python/token.py
+++ python/token.py
@@ -1,4 +1,28 @@
-class TokenType(str)
+class TokenType(str):
+    ILLEGAL = "ILLEGAL"
+    EOF     = "EOF"
+
+    # Identifiers + literals
+    IDENT = "IDENT" # add, foobar, x, y, ...
+    INT   = "INT"   # 1343456
+
+    # Operators
+    ASSIGN   = "="
+    PLUS     = "+"
+
+    # Delimiters
+    COMMA     = ","
+    SEMICOLON = ";"
+
+    LPAREN = "("
+    RPAREN = ")"
+    LBRACE = "{"
+    RBRACE = "}"
+
+    # Keywords
+    FUNCTION = "FUNCTION"
+    LET      = "LET"
+
 
 class Token:
     def __init__(self, type_: TokenType, literal: str):
