c9aba51233800ee3f190381883f6f6e20a1cb195
Alvaro Macedo <alvaro.macedo@nubank.com.br>
2024-12-29 01:10:30 -0300

    Updating lexer

--- scala/interpret/src/main/scala/lexer/Lexer.scala
+++ scala/interpret/src/main/scala/lexer/Lexer.scala
@@ -5,33 +5,72 @@ import token.{Token, TokenType}
 case class Lexer(input: String, position: Int = -1, ch: Byte = 0) {
 
   def readPosition: Int = position + 1
-  def readChar: Lexer = {
-    val newCh: Byte =
-      if (readPosition >= input.length) 0 else input(readPosition).toByte
-    Lexer(input, readPosition, newCh)
+  def readChar: Byte = {
+    if (readPosition >= input.length) 0 else input(readPosition).toByte
   }
-  def nextToken(): Option[Token] = {
-    ch match {
-      case '=' =>
-        Some(Token(TokenType.ASSIGN, ch.toChar.toString))
-      case ';' =>
-        Some(Token(TokenType.SEMICOLON, ch.toChar.toString))
-      case '(' =>
-        Some(Token(TokenType.LPAREN, ch.toChar.toString))
-      case ')' =>
-        Some(Token(TokenType.RPAREN, ch.toChar.toString))
-      case ',' =>
-        Some(Token(TokenType.COMMA, ch.toChar.toString))
-      case '+' =>
-        Some(Token(TokenType.PLUS, ch.toChar.toString))
-      case '{' =>
-        Some(Token(TokenType.LBRACE, ch.toChar.toString))
-      case '}' =>
-        Some(Token(TokenType.RBRACE, ch.toChar.toString))
-      case 0 =>
-        Some(Token(TokenType.EOF, ""))
-      case _ => None
+
+  def readIdentifier: (String, Lexer) = {
+    if (isLetter(ch)) {
+      val readIdentifierNext = next.readIdentifier
+      (ch.toChar.toString ++ readIdentifierNext._1, readIdentifierNext._2)
+    } else {
+      ("", this)
     }
+  }
+
+  def next: Lexer = Lexer(input, readPosition, readChar)
+
+  final val identifierLookupTable: Map[String, TokenType] = Map(
+    "fn" -> TokenType.FUNCTION,
+    "let" -> TokenType.LET
+  )
+  def lookUpIdentifier(identifier: String): TokenType = {
+    identifierLookupTable.getOrElse(identifier, TokenType.IDENT)
+  }
+
+  def isWhitespace(c: Byte): Boolean = {
+    ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r'
+  }
+
+  def nextToken: (Option[Token], Lexer) = {
+    if (isWhitespace(ch)) {
+      next.nextToken
+    } else {
+      ch match {
+        case '=' =>
+          (Some(Token(TokenType.ASSIGN, ch.toChar.toString)), next)
+        case ';' =>
+          (Some(Token(TokenType.SEMICOLON, ch.toChar.toString)), next)
+        case '(' =>
+          (Some(Token(TokenType.LPAREN, ch.toChar.toString)), next)
+        case ')' =>
+          (Some(Token(TokenType.RPAREN, ch.toChar.toString)), next)
+        case ',' =>
+          (Some(Token(TokenType.COMMA, ch.toChar.toString)), next)
+        case '+' =>
+          (Some(Token(TokenType.PLUS, ch.toChar.toString)), next)
+        case '{' =>
+          (Some(Token(TokenType.LBRACE, ch.toChar.toString)), next)
+        case '}' =>
+          (Some(Token(TokenType.RBRACE, ch.toChar.toString)), next)
+        case 0 =>
+          (Some(Token(TokenType.EOF, "")), next)
+        case ch if isLetter(ch) =>
+          val (identifier: String, nextLexer: Lexer) = readIdentifier
+          (
+            Some(
+              Token(lookUpIdentifier(identifier), identifier)
+            ),
+            nextLexer
+          )
+        case _ => (Some(Token(TokenType.ILLEGAL, "")), next)
+      }
+    }
+
+  }
+
+  def isLetter(ch: Byte): Boolean = {
+    'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
   }
 
 }
--- scala/interpret/src/test/scala/lexer/LexerSpec.scala
+++ scala/interpret/src/test/scala/lexer/LexerSpec.scala
@@ -4,7 +4,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 import token.{Token, TokenType}
 
 class LexerSpec extends AnyFlatSpec {
-  "nextToken" should "" in {
+  "nextToken - simple case" should "pass without problems and match all its tokens" in {
     val input = "=+(){},;"
     val expected: Seq[Option[Token]] = Seq(
       Token(TokenType.ASSIGN, "="),
@@ -18,13 +18,12 @@ class LexerSpec extends AnyFlatSpec {
       Token(TokenType.EOF, "")
     ).map(Some(_))
 
-    var lexer: Lexer = Lexer(input)
+    var (_, lexer: Lexer) = Lexer(input).nextToken
 
     expected.foreach { expectedToken =>
       {
-        lexer = lexer.readChar
-        val resultToken = lexer.nextToken()
-
+        val (resultToken: Option[Token], nlexer: Lexer) = lexer.nextToken
+        lexer = nlexer
         (resultToken, expectedToken) match {
           case (Some(t1), Some(t2)) =>
             assert(
@@ -34,9 +33,85 @@ class LexerSpec extends AnyFlatSpec {
           case (None, Some(_)) => fail("Expected token and got EOF")
           case (Some(_), None) => fail("Expected EOF and got token")
         }
-
       }
     }
+  }
+
+  "readIdentifier" should "" in {
+    val input = "input a a a a a"
+    var (_, lexer: Lexer) = Lexer(input).nextToken
+    assert(lexer.nextToken._1.contains(Token(TokenType.IDENT, "input")))
+  }
+
+  "nextToken - more complex case case" should "pass without problems and match all its tokens" in {
+    val input =
+      """let five = 5;
+        let ten = 10;
+
+        let add = fn(x, y) {
+          x + y;
+        };
+
+        let result = add(five, ten);""".stripMargin
+
+    val expected: Seq[Option[Token]] = Seq(
+      Token(TokenType.LET, "let"),
+      Token(TokenType.IDENT, "five"),
+      Token(TokenType.ASSIGN, "="),
+      Token(TokenType.INT, "5"),
+      Token(TokenType.SEMICOLON, ";"),
+      Token(TokenType.LET, "let"),
+      Token(TokenType.IDENT, "ten"),
+      Token(TokenType.ASSIGN, "="),
+      Token(TokenType.INT, "10"),
+      Token(TokenType.SEMICOLON, ";"),
+      Token(TokenType.LET, "let"),
+      Token(TokenType.IDENT, "add"),
+      Token(TokenType.ASSIGN, "="),
+      Token(TokenType.FUNCTION, "fn"),
+      Token(TokenType.LPAREN, "("),
+      Token(TokenType.IDENT, "x"),
+      Token(TokenType.COMMA, ","),
+      Token(TokenType.IDENT, "y"),
+      Token(TokenType.RPAREN, ")"),
+      Token(TokenType.LBRACE, "{"),
+      Token(TokenType.IDENT, "x"),
+      Token(TokenType.PLUS, "+"),
+      Token(TokenType.IDENT, "y"),
+      Token(TokenType.SEMICOLON, ";"),
+      Token(TokenType.RBRACE, "}"),
+      Token(TokenType.SEMICOLON, ";"),
+      Token(TokenType.LET, "let"),
+      Token(TokenType.IDENT, "result"),
+      Token(TokenType.ASSIGN, "="),
+      Token(TokenType.IDENT, "add"),
+      Token(TokenType.LPAREN, "("),
+      Token(TokenType.IDENT, "five"),
+      Token(TokenType.COMMA, ","),
+      Token(TokenType.IDENT, "ten"),
+      Token(TokenType.RPAREN, ")"),
+      Token(TokenType.SEMICOLON, ";"),
+      Token(TokenType.EOF, "")
+    ).map(Some(_))
+
+    var (_, lexer: Lexer) = Lexer(input).nextToken
+
+    expected.foreach { expectedToken =>
+      {
+        val (resultToken: Option[Token], nlexer: Lexer) = lexer.nextToken
+        lexer = nlexer
+        (resultToken, expectedToken) match {
+          case (Some(t1), Some(t2)) =>
+            assert(
+              t1.tokenType == t2.tokenType && t1.literal == t2.literal,
+              s"expected token $t2 and got $t1"
+            )
+          case (None, Some(_)) => fail("Expected token and got EOF")
+          case (Some(_), None) => fail("Expected EOF and got token")
+        }
+      }
+    }
+
   }
 
 }
