bd44efd6de1d9a562cdc1309a5100ff263b59c07
alvaro.macedo <alvaro.macedo@nubank.com.br>
2025-01-28 17:01:20 -0300

    Updating block statements strings

--- scala/interpret/src/main/scala/parser/ast/statements/BlockStatement.scala
+++ scala/interpret/src/main/scala/parser/ast/statements/BlockStatement.scala
@@ -4,12 +4,12 @@ import parser.ast.{Expression, Statement}
 import token.Token
 
 case class BlockStatement(
-                           token: Token,
-                           statements: Seq[Statement]
-                         ) extends Statement {
+    token: Token,
+    statements: Seq[Statement]
+) extends Statement {
   override def statementNode(): Unit = {}
 
   override def tokenLiteral: String = token.literal
 
-  override def string: String = statements.map(_.string).mkString
+  override def string: String = s"{ ${statements.map(_.string).mkString} }"
 }
