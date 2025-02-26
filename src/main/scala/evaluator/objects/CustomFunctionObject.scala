07d1e4c228c8b4053806fb480c5a0180bfc228f8
Álvaro Marques Macedo <sc.poi458@gmail.com>
2025-02-25 22:22:17 -0300

    Fix function base (#27)

--- src/main/scala/evaluator/objects/CustomFunctionObject.scala
+++ src/main/scala/evaluator/objects/CustomFunctionObject.scala
@@ -4,7 +4,7 @@ import evaluator.{Anything, ObjectType}
 import parser.ast.expressions.Identifier
 import parser.ast.statements.BlockStatement
 
-case class CustomFunctionObject(parameters: Seq[Identifier], body: BlockStatement) extends Anything {
+case class CustomFunctionObject(parameters: Seq[Identifier], body: BlockStatement) extends FunctionObject {
   override def objectType: ObjectType = ObjectType.Function
   override def inspect: String = this.toString
 }
